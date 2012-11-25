28c21511a3743423d0804ecb14de955c1e4f9a0e
fanyingming <yingmingfan@qq.com>
2012-11-25 13:36:31 +0800

    只是加入Dao包，不做其他配置，可以运行。
    Signed-off-by: fanyingming <yingmingfan@qq.com>

---
+++ src/com/imu/coursenet/dao/impl/AdminDaoImpl.java
@@ -0,0 +1,67 @@
+package com.imu.coursenet.dao.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.imu.coursenet.dao.AdminDao;
+import com.imu.coursenet.domain.Admin;
+import com.imu.coursenet.support.YeekuHibernateDaoSupport;
+
+public class AdminDaoImpl 
+	extends YeekuHibernateDaoSupport 
+	implements AdminDao
+{
+
+	@Override
+	public Admin get(Integer userId) {
+		return getHibernateTemplate()
+				.get(Admin.class , userId);
+	}
+
+	@Override
+	public Integer save(Admin admin) {
+		return (Integer)getHibernateTemplate()
+				.save(admin);
+	}
+
+	@Override
+	public void update(Admin admin) {
+		getHibernateTemplate()
+		.update(admin);
+		
+	}
+
+	@Override
+	public void delete(Admin admin) {
+		getHibernateTemplate()
+		.delete(admin);
+		
+	}
+
+	@Override
+	public void delete(Integer userId) {
+		getHibernateTemplate()
+		.delete(get(userId));
+		
+	}
+
+	@Override
+	public List<Admin> findAll() {
+		return (List<Admin>)getHibernateTemplate()
+				.find("from Admin");
+	}
+	
+	@Override
+	public List<Admin> findByAccountAndPass(String userAccount,String userPass)
+	{/*
+		return (List<Admin>)getHibernateTemplate()
+			.find("from Admin a where a.userAccount = ? and a.userPass=?"
+			,userAccount , userPass); 
+			*/
+		List admin=new ArrayList();
+		Admin ad=new Admin();
+		admin.add(ad);
+		return admin;
+	}
+
+}
