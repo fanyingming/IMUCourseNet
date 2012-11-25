9335d868f220ee9631cd70c63b2117bf83b504f5
fanyingming <yingmingfan@qq.com>
2012-11-25 13:46:52 +0800

    将DAO包融入Spring，只是Dao不操作数据库，可以运行。
    Signed-off-by: fanyingming <yingmingfan@qq.com>

--- src/com/imu/coursenet/service/impl/UserManagerImpl.java
+++ src/com/imu/coursenet/service/impl/UserManagerImpl.java
@@ -4,12 +4,22 @@ import com.imu.coursenet.domain.*;
 import com.imu.coursenet.service.*;
 
 public class UserManagerImpl implements UserManager{
-
+	private AdminDao adminDao;
 	
+	
+	public void setAdminDao(AdminDao adminDao) {
+		this.adminDao = adminDao;
+	}
+
+
 	@Override
 	public int validLogin(String userAccount,String userPass) {
-		
-		return LOGIN_ADMIN ;
+		if(userAccount.length()==0 || userPass.length()==0) 
+			return this.LOGIN_FAIL;
+		else if(adminDao.findByAccountAndPass(userAccount, userPass).size()==1)
+				return LOGIN_ADMIN ;
+		else
+			return this.LOGIN_FAIL;
 	}
 
 }
