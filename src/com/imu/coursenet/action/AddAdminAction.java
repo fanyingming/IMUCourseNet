d82b65c94303adab616fbc0f568928dd6bcb9c88
fanyingming <yingmingfan@gmail.com>
2012-11-27 20:55:26 +0800

    刚才把AddAdminAction不小心删除了，补回来了。

--- src/com/imu/coursenet/action/AddAdminAction.java
+++ src/com/imu/coursenet/action/AddAdminAction.java
@@ -1,7 +1,26 @@
 package com.imu.coursenet.action;
 
+import java.util.List;
+
 import com.imu.coursenet.action.base.UserManagerBaseAction;
+import com.imu.coursenet.domain.*;
 
 public class AddAdminAction extends UserManagerBaseAction{
+	private List<Department> departments;
 
+	public List<Department> getDepartments() {
+		return departments;
+	}
+
+	public void setDepartments(List<Department> departments) {
+		this.departments = departments;
+	}
+
+	@Override
+	public String execute() throws Exception {
+		departments=departmentManager.listAllDepartment();
+		return SUCCESS;
+	}
+	
+	
 }
